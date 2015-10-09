aa2fd3c677ba31999882fa44f6f964d15d9fcf8f
Andrew Goodrick–Werner <andrew.gw@me.com>
2015-10-09 07:22:59 -0400

    Email

--- M2/application/controllers/Contact.php
+++ M2/application/controllers/Contact.php
@@ -116,23 +116,23 @@ class Contact extends CI_Controller
 		$this->load->library('email');
 
 		$this->email->from($email, $name);
-			$this->email->to('andrew.gw@me.com');
-			$this->email->subject('SleepyMe Comment');
-			$this->email->message(
-				"ip: " . $this->input->ip_address() .
-				"\r\nname: " . $this->input->post('name') .
-				"\r\npostal: " . $this->input->post('postal') .
-				"\r\nphone: " . $this->input->post('phone') .
-				"\r\ncomment: " . $this->input->post('comment')
-				);
+		$this->email->to('000306746@csunix.mohawkcollege.ca', 'andrew.gw@me.com');
+		$this->email->subject('SleepyMe Comment');
+		$this->email->message(
+			"ip: " . $this->input->ip_address() .
+			"\r\nname: " . $this->input->post('name') .
+			"\r\npostal: " . $this->input->post('postal') .
+			"\r\nphone: " . $this->input->post('phone') .
+			"\r\ncomment: " . $this->input->post('comment')
+		);
 
-			if ($this->email->send() == FALSE) {
-				$this->TPL['errors'] = true;
-				$this->TPL['msg'] = "We didn't receive your message for some reason.";
-			} else {
-				$this->TPL['submitted'] = true;
-				$this->TPL['msg'] = "Thanks for your feedback!";
-			}
+		if (! $this->email->send()):
+			$this->TPL['errors'] = true;
+			$this->TPL['msg'] = "We didn't receive your message for some reason.";
+		else:
+			$this->TPL['submitted'] = true;
+			$this->TPL['msg'] = "Thanks for your feedback!";
+		endif;
 
 		$this->template->show('contact', $this->TPL);
 	}
